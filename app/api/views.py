b783845814428691ae692f82cafa8e6b310aff3f
lvcky-gg <matadorcrab@gmail.com>
2023-09-05 17:02:05 -0500

    gather data

--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,63 @@
 from django.shortcuts import render
+import requests
+import json
+teams = requests.get("https://statsapi.mlb.com/api/v1/teams?sportId=1")
+Standings = requests.get("https://statsapi.mlb.com/api/v1/standings?leagueId=103,104").json()["records"]
 
+    # print(teams.json()["teams"][0]["division"])
+alEast = [[i["id"], i["name"], i["link"], i["league"]] for i in teams.json()["teams"] if i["division"]["name"] == "American League East"]
+nlEast = [[i["id"], i["name"], i["link"]] for i in teams.json()["teams"] if i["division"]["name"] == "National League East"]
+alCentral = [[i["id"], i["name"], i["link"]] for i in teams.json()["teams"] if i["division"]["name"] == "American League Central"]
+nlCentral = [[i["id"], i["name"], i["link"]] for i in teams.json()["teams"] if i["division"]["name"] == "National League Central"]
+alWest = [[i["id"], i["name"], i["link"]] for i in teams.json()["teams"] if i["division"]["name"] == "American League West"]
+nlWest = [[i["id"], i["name"], i["link"]] for i in teams.json()["teams"] if i["division"]["name"] == "National League West"]
 # Create your views here.
+
+# alEastStandings = [[i["team"], i["records"]] for i in Standings ]
+
+alEastNames = [i[1] for i in alEast]
+nlEastNames = [i[1] for i in nlEast]
+alCentralNames = [i[1] for i in alCentral]
+nlCentralNames = [i[1] for i in nlCentral]
+alWestNames = [i[1] for i in alWest]
+nlWestNames = [i[1] for i in nlWest]
+
+
+# POLYAS
+# Need a list of each team in a division
+# need a List each containing the following: Name of Team, Team ID, Team Link, win, loss, pct, GB, L10, DIFF
+
 def home(response):
-    return render(response, "index.html", {})
+    ale = []
+    nle = []
+    alc = []
+    nlc = []
+    alw = []
+    nlw = []
+    for i in range(len(Standings)):
+        teamStanding = Standings[i]['teamRecords']
+
+        
+        for k in range(len(teamStanding)):
+            team = {"team":teamStanding[k]['team'],"leagueRecord":teamStanding[k]['leagueRecord']}
+            if teamStanding[k]['team']['name'] in alEastNames:
+                ale.append(team)
+            elif teamStanding[k]['team']['name'] in nlEastNames:
+                nle.append(team)
+            elif teamStanding[k]['team']['name'] in alCentralNames:
+                alc.append(team)
+            elif teamStanding[k]['team']['name'] in nlCentralNames:
+                nlc.append(team)
+            elif teamStanding[k]['team']['name'] in alWestNames:
+                alw.append(team)
+            elif teamStanding[k]['team']['name'] in nlWestNames:
+                nlw.append(team)
+
+    
+
+       
+       
+    return render(response, "home.html", {'divisions':[{"name":"AL East","div":ale},{"name":"NL East","div":nle},{"name":"AL Central","div":alc},{"name":"NL Central","div":nlc},{"name":"AL West","div":alw},{"name":"NL West","div":nlw}]})
+
+    # print(alEastStandings.json()["records"][0]["teamRecords"])
+    
