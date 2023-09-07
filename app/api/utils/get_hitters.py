f73af4c0a36c50c7a4e1becc0dbebc9bb4d40942
lvcky-gg <matadorcrab@gmail.com>
2023-09-07 18:52:01 -0500

    get hitters

---
+++ app/api/utils/get_hitters.py
@@ -0,0 +1,36 @@
+import requests
+
+
+def get_hitters(id):
+    players = []
+    val = requests.get(f"https://statsapi.mlb.com/api/v1/teams/{id}/roster/Active?hydrate=person(stats(type=season))").json()["roster"]
+    for i in range(len(val)):
+        
+        if val[i]["position"]["abbreviation"] != "P" and val[i]["position"]["abbreviation"] != "C":
+            
+            value = {
+                "id":val[i]["person"]["id"],
+                "position":val[i]["position"]["abbreviation"],
+                "fullname":val[i]["person"]["fullName"],
+                "jerseyNumber":val[i]["jerseyNumber"],
+                "name":val[i]["person"]["fullName"],
+                "age":val[i]["person"]["currentAge"],
+                "height":val[i]["person"]["height"],
+                "weight":val[i]["person"]["weight"],
+                "birthDate":val[i]["person"]["birthDate"],
+                "b":val[i]["person"]["batSide"]["code"],
+                "t":val[i]["person"]["pitchHand"]["code"],
+                "pa":val[i]["person"]["stats"][0]["splits"][0]["stat"]["plateAppearances"],
+                "h":val[i]["person"]["stats"][0]["splits"][0]["stat"]["hits"],
+                "2b":val[i]["person"]["stats"][0]["splits"][0]["stat"]["doubles"],
+                "3b":val[i]["person"]["stats"][0]["splits"][0]["stat"]["triples"],
+                "hr":val[i]["person"]["stats"][0]["splits"][0]["stat"]["homeRuns"],
+                "sb":val[i]["person"]["stats"][0]["splits"][0]["stat"]["stolenBases"],
+                "avg":val[i]["person"]["stats"][0]["splits"][0]["stat"]["avg"],
+                "obp":val[i]["person"]["stats"][0]["splits"][0]["stat"]["obp"],
+                "ops":val[i]["person"]["stats"][0]["splits"][0]["stat"]["ops"],
+            }
+    
+        
+            players.append(value)
+    return players
--- app/api/views.py
+++ app/api/views.py
@@ -4,6 +4,7 @@ import requests
 from .utils.fetch_teams import create_teams
 from .utils.fetch_news import parseHTML
 from .utils.team_details import team_details
+from .utils.get_hitters import get_hitters
 
 
 
@@ -19,9 +20,5 @@ def home(response):
     
 def team_hitters(request, id=id):
     teamdetails = team_details(id)
-    players = requests.get(f"https://statsapi.mlb.com/api/v1/teams/{id}/roster/Active?hydrate=person(stats(type=season))").json()["roster"]
-    
-    print(players[0])
-        
-    
+    players = get_hitters(id) 
     return render(request, "team_hitters.html", { "id":id, "team":teamdetails, "players":players }) 
