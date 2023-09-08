d1b5d35f952f2aca8ea307a198f9d0300a32de28
lvcky-gg <matadorcrab@gmail.com>
2023-09-07 21:44:01 -0500

    edge case management

--- app/api/utils/get_hitters.py
+++ app/api/utils/get_hitters.py
@@ -7,6 +7,36 @@ def get_hitters(id):
     for i in range(len(val)):
         
         if val[i]["position"]["abbreviation"] != "P":
+            pa = 0
+            h = 0
+            doubles = 0
+            triples = 0
+            hr = 0
+            sb = 0
+            avg = 0
+            obp = 0
+            ops = 0
+            
+            if 'stats' in val[i]["person"]:
+                stats = val[i]["person"]["stats"][0]["splits"][0]["stat"]
+                if 'plateAppearances' in stats:
+                    pa = stats["plateAppearances"]
+                if 'hits' in stats:
+                    h = stats["hits"]
+                if 'doubles' in stats:
+                    doubles = stats["doubles"]
+                if 'triples' in stats:
+                    triples = stats["triples"]
+                if 'homeRuns' in stats:
+                    hr = stats["homeRuns"]
+                if 'stolenBases' in stats:
+                    sb = stats["stolenBases"]
+                if 'avg' in stats:
+                    avg = stats["avg"]
+                if 'obp' in stats:
+                    obp = stats["obp"]
+                if 'ops' in stats:
+                    ops = stats["ops"]
             
             value = {
                 "id":val[i]["person"]["id"],
@@ -20,15 +50,15 @@ def get_hitters(id):
                 "birthDate":val[i]["person"]["birthDate"],
                 "b":val[i]["person"]["batSide"]["code"],
                 "t":val[i]["person"]["pitchHand"]["code"],
-                # "pa":val[i]["person"]["stats"][0]["splits"][0]["stat"]["plateappearances"],
-                "h":val[i]["person"]["stats"][0]["splits"][0]["stat"]["hits"],
-                "2b":val[i]["person"]["stats"][0]["splits"][0]["stat"]["doubles"],
-                "3b":val[i]["person"]["stats"][0]["splits"][0]["stat"]["triples"],
-                "hr":val[i]["person"]["stats"][0]["splits"][0]["stat"]["homeRuns"],
-                "sb":val[i]["person"]["stats"][0]["splits"][0]["stat"]["stolenBases"],
-                "avg":val[i]["person"]["stats"][0]["splits"][0]["stat"]["avg"],
-                "obp":val[i]["person"]["stats"][0]["splits"][0]["stat"]["obp"],
-                "ops":val[i]["person"]["stats"][0]["splits"][0]["stat"]["ops"],
+                "pa":pa,
+                "h":h,
+                "2b":doubles,
+                "3b":triples,
+                "hr":hr,
+                "sb":sb,
+                "avg":avg,
+                "obp":obp,
+                "ops":ops,
             }
     
         
