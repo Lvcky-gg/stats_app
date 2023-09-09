529269da33512c28e87042366bef14614872c592
lvcky-gg <matadorcrab@gmail.com>
2023-09-09 00:42:53 -0500

    r

--- settings.py
+++ settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'asdweasdwASDq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
