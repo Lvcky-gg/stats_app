ebd62195151cdeb67e3f3bdf5ed393e3ba2ed82b
lvcky-gg <matadorcrab@gmail.com>
2023-09-10 06:33:14 -0500

    added static

--- settings.py
+++ settings.py
@@ -61,13 +61,14 @@ STATICFILES_DIRS = (
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'urls'
