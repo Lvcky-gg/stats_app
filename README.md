0d51a4f7157c7697a7e19bb8542075becc5aed28
lvcky-gg <johnodonnell1997@icloud.com>
2023-09-10 19:47:32 -0500

    init readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Stats App
 
+
+## Start
+- 0. Make a .env file @ root and copy contents of env.example into it
+- 1. pipenv shell
+- 2. ./build.sh
+- 3. ./start_prod.sh
+
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 
-pip install -r requirements.txt
+pipenv install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
